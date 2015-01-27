caa533b707d0330051118e1127cf1345275b0d21
el-ethan <elethan@gmail.com>
2015-01-26 22:47:26 -0600

    Changed for loop to be more 'Pythonic'
    Previously I used range() to loop through and
    access chapter codes by index. Now it is done
    simply by looking 'for code in chapter_codes'.

--- xs_getter.py
+++ xs_getter.py
@@ -21,16 +21,16 @@ toc_html = response.read()
 re_find_chapters = re.compile('(?<=><a href=")\d+(?=\.)')
 chapter_codes = re_find_chapters.findall(toc_html)
 
+
 # Create new urls
-for code in range(0, len(chapter_codes)):
-    new_url = url + chapter_codes[code] + ".html"
+for code in chapter_codes:
+    new_url = url + code + ".html"
     new_response = urllib2.urlopen(new_url)
     html = new_response.read()
-    cleaner_html = re.sub('(?<=<a)|\n.*(?=</a)', "", html)
+    cleaner_html = re.sub('(?<=<a)|\n.*(?=</a)', '', html)
     soup = BeautifulSoup(cleaner_html)
     pro_soup = soup.get_text()
-    cleaner_soup = re.sub('<!--.*-->', "", pro_soup, flags = re.DOTALL)
-
+    cleaner_soup = re.sub('<!--.*-->', '', pro_soup, flags=re.DOTALL)
 
     # open new text file to fill with novel text
     with open("xiaoshuo.txt", 'a') as f:
