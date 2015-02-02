cbbc7c6fbd42c5d6a83bd71272b9e8c54474abc9
el-ethan <elethan@gmail.com>
2015-02-01 22:42:36 -0600

    xs_get now uses functions from separate module
    HTML is now processed by homegrown functions;
    no more BeautifulSoup.

--- xs_get.py
+++ xs_get.py
@@ -7,9 +7,10 @@ Currently the url should be from 努努书坊.
 """
 import re
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from html_clnr import *
 
-def html_fixer(html, codec):
+
+def html_decoder(html, codec):
     """decode html and return string"""
     html = html.decode(codec)
     return str(html)
@@ -19,7 +20,7 @@ url =  'http://www.kanunu8.com/book3/7192/'     # For testing purposes
 # input("Please enter a URL:").replace("index.html", "")
 response = urlopen(url)
 html = response.read()
-html = html_fixer(html, codec='gb18030')
+html = html_decoder(html, codec='gb18030')
 
 # Use regex to find chapter codes in html, save as list
 re_find_chapters = re.compile('(?<=><a href=")\d+(?=\.)')
@@ -29,27 +30,17 @@ chapter_codes = re_find_chapters.findall(html)
 m = re.search('(?<=<title>).*(?=</title>)', html)
 title = m.group()
 
-# Use regex to find extraneous text and code to be removed later
-junk = re.compile('(?<=<a)|\n.*(?=</a)')
-html_comm = re.compile('<!--.*-->', flags=re.DOTALL)
-extra_lines = re.compile('\n{4,}')
-
-# Create new urls and make soup
+# Create new urls
 for code in chapter_codes:
     new_url = url + code + ".html"
     new_response = urlopen(new_url)
     html = new_response.read()
-    html = html_fixer(html, codec='gb18030')
-    # Remove extraneous text that get_text below doesn't remove
-    clean_html = re.sub(html_comm, '', html)
-    cleaner_html = re.sub(junk, '', clean_html)
-    soup = BeautifulSoup(cleaner_html)
-    text = soup.get_text()
+    html = html_decoder(html, codec='gb18030')
 
-    # Clean up text
-    rm_lines = re.sub(extra_lines, '\n\n', text)
-    clean_text = rm_lines.replace(u'->正文', '').replace(u'努努书坊 版权所有', '')
+    # Remove extraneous text that get_text below doesn't remove
+
+    text = html_clnr(html, site='nunu')
 
     # open new text file to fill with novel text
     with open("%s.txt" % title, 'ab') as f:
-        f.write(clean_text.encode('utf-8'))
+        f.write(text.encode('utf-8'))
