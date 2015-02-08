f43dca6bd6bcae8f18cfd7b3ae9e8f745fe40585
el-ethan <elethan@gmail.com>
2015-02-08 10:19:38 -0600

    Update tests

--- bookfish.py
+++ bookfish.py
@@ -9,6 +9,7 @@ def bookfish(url, site='nunu', codec='gb18030', print_to_file=False):
     with the contents of the novel will be created
     as 'title of novel.txt'
     """
+    # Get url tail
     # Get html
     html = urlopen(url).read().decode(codec)
     # Get chapters
@@ -18,9 +19,15 @@ def bookfish(url, site='nunu', codec='gb18030', print_to_file=False):
     re_find_title = re.search('(?<=<title>).*(?=</title>)', html)
     title = re_find_title.group()
     # Get html of novel
+    url_tails = {'nunu': '.html',
+                 'ifeng': '.htm'
+    }
     html_novel = ''
     for code in chapter_codes:
-        new_url = url + code + ".html"
+        url_base = re.sub('\.htm$|\.html$', '', url)
+        if site == 'ifeng':
+            url_base += '/'
+        new_url = url_base + code + url_tails[site]
         html = urlopen(new_url).read().decode(codec)
         html_novel += '#'*20 + html
 
--- tests.py
+++ tests.py
@@ -1,21 +1,18 @@
 """Various tests of bookfish."""
 import unittest
-import fishfood
 import bookfish
 from urllib.request import urlopen
 
-test_url = 'http://www.kanunu8.com/book3/7192/'
-test_html = urlopen(test_url).read().decode('gb18030')
-class TestFood(unittest.TestCase):
 
-    def test_get_title(self):
-        self.assertEqual(bookfish.get_title(test_html),
-                        '古典爱情 - 余华 - 小说在线阅读 - 努努书坊',
-                        "Title doesn't match")
-    def test_get_html(self):
-        self.assertEqual(bookfish.get_html(test_url),
-                        test_html,
-                        "HTML doesn't match")
+class TestFish(unittest.TestCase):
+
+    def test_made_novel(self):
+        test_url = 'http://www.kanunu8.com/book3/7192/'
+        f = open('xiaoshuo.txt', 'r')
+        text = f.read()
+        f.close()
+        self.assertEqual(bookfish.bookfish(test_url), text,
+                         "Output does not match test novel")
 
 if __name__ == "__main__":
     unittest.main()
