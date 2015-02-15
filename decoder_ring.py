0bfb886eb213fdfa3930e4cef886cc51210f36db
el-ethan <elethan@gmail.com>
2015-02-14 20:58:04 -0600

    Add actual functionality to decoder_ring
    Now this module can translate mojibake into
    the correct characters. Still needs a lot of
    work; right now you have to sift through
    hundreds of lines of input to find the decoded
    phrase.

--- decoder_ring.py
+++ decoder_ring.py
@@ -1,25 +1,24 @@
-def decoder_ring(text, codec=None):
-    """decode text and return string"""
+import random
 
-    codecs = [
-                'utf_8','gb18030', 'gbk',           # Unified
-                'gb2312', 'hz', 'iso2022_jp_2',     # Simplified
-                'big5hkscs', 'big5', 'cp950',       # Traditional
+codecs = ['ascii', 'big5', 'big5hkscs', 'cp037', 'cp424', 'cp437', 'cp500', 'cp737', 'cp775', 'cp850', 'cp852', 'cp855', 'cp856', 'cp857', 'cp860', 'cp861', 'cp862', 'cp863', 'cp864', 'cp865', 'cp866', 'cp869', 'cp874', 'cp875', 'cp932', 'cp949', 'cp950', 'cp1006', 'cp1026', 'cp1140', 'cp1250', 'cp1251', 'cp1252', 'cp1253', 'cp1254', 'cp1255', 'cp1256', 'cp1257', 'cp1258', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213', 'euc_kr', 'gb2312', 'gbk', 'gb18030', 'hz', 'iso2022_jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_2004', 'iso2022_jp_3', 'iso2022_jp_ext', 'iso2022_kr', 'latin_1', 'iso8859_2', 'iso8859_3', 'iso8859_4', 'iso8859_5', 'iso8859_6', 'iso8859_7', 'iso8859_8', 'iso8859_9', 'iso8859_10', 'iso8859_13', 'iso8859_14', 'iso8859_15', 'johab', 'koi8_r', 'koi8_u', 'mac_cyrillic', 'mac_greek', 'mac_iceland', 'mac_latin2', 'mac_roman', 'mac_turkish', 'ptcp154', 'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'utf_16', 'utf_16_be', 'utf_16_le', 'utf_7', 'utf_8']
+
+cjk_codecs = [ 'big5', 'big5hkscs', 'cp932', 'cp949', 'cp950', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213', 'euc_kr', 'gb2312', 'gbk', 'gb18030', 'hz', 'iso2022_jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_2004', 'iso2022_jp_3', 'iso2022_jp_ext', 'iso2022_kr', 'johab', 'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'utf_16', 'utf_16_be', 'utf_16_le', 'utf_7', 'utf_8'
+]
+
+mjbk = '¸æËßÎÒ'
+
+for encoder in codecs:
+    try:
+        mj = bytes(mjbk, encoding=encoder)
+    except UnicodeEncodeError:
+        continue
+    for decoder in cjk_codecs:
+            try:
+                if mj != mj.decode(decoder):
+                    print(mj.decode(decoder))
+                    print('encoding: ' + encoder)
+                    print('decoding: ' + decoder)
+            except UnicodeDecodeError:
+                continue
 
 
-    ]
-
-    for codec in codecs:
-
-        try:
-            text = bytes(text, encoding='utf_8').decode(codec)
-        except UnicodeDecodeError:
-            print("Codec (%s) failed. Trying next codec..." % codec)
-        else:
-            print("\"%s\" decoded from %s" % (text, codec))
-
-
-
-if __name__ == "__main__":
-    text = '¿¡ÇÇÅæ ÇÁ·ÎÁ§Æ®'
-    print(decoder_ring(text))
