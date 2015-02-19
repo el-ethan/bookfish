7f8789d326b8bffb9044d86c14c1e0f619df7bcc
el-ethan <elethan@gmail.com>
2015-02-18 21:25:59 -0600

    Change tests to work with new version of bookfish

--- tests.py
+++ tests.py
@@ -12,19 +12,30 @@ class TestFish(unittest.TestCase):
     def test_made_novel(self):
         test_url = 'http://www.kanunu8.com/book3/7192/'
         f = open('nunu_test.txt', 'r')
-        text = f.read()
+        expected_book = f.read()
         f.close()
-        self.assertEqual(bookfish.bookfish(test_url), text,
+        observed_book = bookfish.Bookfish(test_url).book
+        self.assertEqual(observed_book, expected_book,
                          "Output does not match test novel")
+    # def test_made_file(self):
+    #     test_url = 'http://www.kanunu8.com/book3/7192/'
+    #     f = open('nunu_test.txt', 'r')
+    #     expected_book = f.read()
+    #     f.close()
+    #     xs = bookfish.Bookfish(test_url)
+    #     xs.make_file()
 
-class TestDecoderRing(unittest.TestCase):
+    #     self.assertEqual(observed_book, expected_book,
+    #                      "Output does not match test novel")
 
-    def test_bake_to_moji(self):
-        """Verify that mojibake is correctly converted to characters"""
-        mjbk = '¸æËßÎÒ'
-        mj = '告诉我'
-        moji = mjbk.decoder_ring(mjbk)
-        self.assertIn(mj, moji)
+# class TestDecoderRing(unittest.TestCase):
+
+#     def test_bake_to_moji(self):
+#         """Verify that mojibake is correctly converted to characters"""
+#         mjbk = '¸æËßÎÒ'
+#         mj = '告诉我'
+#         moji = mjbk.decoder_ring(mjbk)
+#         self.assertIn(mj, moji)
 
 if __name__ == "__main__":
     unittest.main()
