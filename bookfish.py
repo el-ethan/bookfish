b4b4ec5f03eeea68acc8bfd88f4f67e5dcc0ec01
el-ethan <elethan@gmail.com>
2015-02-28 15:09:19 -0600

    Add more documentation to bookfish

--- bookfish.py
+++ bookfish.py
@@ -37,12 +37,27 @@ except ImportError:
 
 
 class Bookfish():
+    """Bookfish creates a readable text representation of novel from html
 
+    Feed Bookfish the URL of index/Table of contents page for a Chinese
+    novel from a supported site. The resulting Bookfish object has the
+    following methods and attributes:
+
+    * html_book: the raw html of the entire novel
+    * title: the title of the novel (this is used as the file name when
+      make_file() is called)
+    * url: the original url of index page
+    * book: the complete text of the novel
+
+    Also, you can call the object's make_file() method to save the novel into
+    a txt file in the cwd. The name of the file will be the same as the
+    object's title attribute.
+    """
     sites = {'nunu':{'junk': '<a.*?</a>'
                              '|<.*?>'
                              '|<!--(.*\n)+?-->'
                              '|&.*?;',
-                    'tails': '(?<=<a href=")\d+.html?'},
+                    'extens': '(?<=<a href=")\d+.html?'},
             'hexun':{'junk': '<a.*?</a>'
                              '|\n(.*</script>)'
                              '|<.*?\s?>'
@@ -55,32 +70,33 @@ class Bookfish():
                              '|<iframe.*'
                              '|//.*'
                              '|..onclick=.*',
-                    'tails': '(?<=<a href="/)chapter[-\d\w]+.shtml'},
+                    'extens': '(?<=<a href="/)chapter[-\d\w]+.shtml'},
             'yanqing888':{'junk': '',
-                    'tails': '(?<=<a href=")\d+.html'},
+                    'extens': '(?<=<a href=")\d+.html'},
             'dddbbb':{'junk': '',
-                      'tails': '(?<=<a href=")/\d+_\d+\.html'},
+                      'extens': '(?<=<a href=")/\d+_\d+\.html'},
     }
 
     codecs = ['gb18030', 'gb2312', 'gbk', 'big5']
 
     def __init__(self, url):
-
         self.url = url
         self.site_name = ''
         for sn in self.sites.keys():
             if sn in self.url:
-                self.site_name = sn
-        self.html = self.get_html(self.url)
-        self.title = self.get_title()
-        self.html_book = self.get_html_book()
-        self.book = self.get_book()
+                self.site_name = sn             # Name of source site
+        self.html = self.get_html(self.url)     # HTML of index/TOC page
+        self.title = self.get_title()           # Title of novel
+        self.html_book = self.get_html_book()   # HTML of entire novel
+        self.book = self.get_book()             # Full text of novel
 
     def codec_generator(self):
+        """Generate codecs to decode Chinese text in html"""
         for codec in self.codecs:
             yield codec
 
     def get_html(self, url):
+        """Get html from url and decode Chinese text"""
         codec = self.codec_generator()
         while True:
             try:
@@ -91,33 +107,40 @@ class Bookfish():
         return html
 
     def get_title(self):
+        """Return title of novel with regex"""
         re_find_title = re.search('(?<=<title>).*(?=</title>)', self.html)
         title = re_find_title.group()
         return title
 
     def get_html_book(self):
-        re_find_tails = re.compile(self.sites[self.site_name]['tails'])
-        url_tails = re_find_tails.findall(self.html)
-        # Get html of novel
+        """Return html of complete novel with regex"""
+        # Use regex to determine extensions at end of url (html, shtml, etc.)
+        re_find_extens = re.compile(self.sites[self.site_name]['extens'])
+        url_extens = re_find_extens.findall(self.html)
+        # Combine url elements to form urls for each chapter
         html_book = ''
-        for tail in url_tails:
+        for tail in url_extens:
+            # Determine what needs to be stripped from end of url to make base
             url_base = re.sub('\.html?$'            # nunu, yanqing888
                               '|book-\d+.shtml'     # hexun
                               '|/html/.*opf.html',  # dddbbb
                               '', self.url)
             new_url = url_base + tail
             html = self.get_html(new_url)
+            # Add row of 20 hash signs between chapters
             html_book += '#'*20 + html
         return html_book
 
     def get_book(self):
+        """Return full text of novel by cleaning up html with regex"""
+        # Remove html tags and extraneous text from html
         text = re.sub(self.sites[self.site_name]['junk'], '', self.html_book)
         # self.site_name specific junk to remove
         leftover_junk = {'nunu': ['--正文', '努努书坊 版权所有','|'
-                     ],
-                     'hexun': ['if(w_frame.readyState)', 'else', '*/', '/*'
-                     ],
-                     'dddbbb': ['|', '>']
+                        ],
+                        'hexun': ['if(w_frame.readyState)', 'else', '*/', '/*'
+                        ],
+                        'dddbbb': ['|', '>']
         }
         # Extra newlines (2 or more consecutive)
         extra_lines = re.compile('\s{2,}')
@@ -126,7 +149,6 @@ class Bookfish():
             text = text.replace(j, '')
         # Remove extra whitespace
         text = re.sub(extra_lines, '\n\n', text)
-        # text += '\n' + u'\U0001F41F'*20
         return text
 
     def make_file(self):
@@ -135,6 +157,23 @@ class Bookfish():
 
 
 class TwoFish(Bookfish):
+    """TwoFish creates text representation of novel from html with Python 2.7
+
+    Use this class if you are using Python 2.X. Otherwise use Bookfish
+    for 3.X. Feed TwoFish the URL of index/Table of contents page for a
+    Chinese novel from a supported site. The resulting TwoFish object
+    has the following methods and attributes:
+
+    * html_book: the raw html of the entire novel
+    * title: the title of the novel (this is used as the file name when
+      make_file() is called)
+    * url: the original url of index page
+    * book: the complete text of the novel
+
+    Also, you can call the object's make_file() method to save the novel into
+    a txt file in the cwd. The name of the file will be the same as the
+    object's title attribute.
+    """
     fish = '><(((º>TwoFish<º)))><'
 
     def get_html(self, url):
@@ -146,17 +185,3 @@ class TwoFish(Bookfish):
             except UnicodeDecodeError:
                 continue
         return html
-
-# if __name__ == "__main__":
-#     url = input("Enter a URL:")
-#     if not url:
-#         url = 'http://data.book.hexun.com/book-19250.shtml'
-#     print(bookfish(url))
-
-# novel = Bookfish('http://data.book.hexun.com/book-19250.shtml')
-# print(novel.url)
-# print(novel.site_name)
-# print(novel.fishy())
-# # print(novel.get_html())
-# print(novel.get_title())
-# print(novel.book)
