809b0abdaf5a778c72d57d36eddab008dd46bad0
el-ethan <elethan@gmail.com>
2015-03-26 22:22:55 -0500

    bookfish once again implemented with class

--- bookfish.py
+++ bookfish.py
@@ -1,6 +1,34 @@
+#!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 """Bookfish"""
+import re
+import urllib.request
 
-def find_chapter_urls(html):
-    """Take url and return chapters"""
-    return []
+
+class Bookfish(object):
+
+    def __init__(self, url):
+        self.url = url
+        self.html = self.get_html()
+        self.chapters = self.find_chapter_urls()
+
+    def get_html(self):
+        f = urllib.request.urlopen(self.url)
+        html = f.read()
+        html = html.decode('gb18030')
+        return html
+
+    def find_chapter_urls(self):
+        """Take url and return chapters"""
+        regex = re.compile(r'(?<=href=")\d+\.html')
+        m = re.findall(regex, self.html)
+        chapter_urls = []
+        for chapter in m:
+            chapter_urls.append(self.url + chapter)
+        return chapter_urls
+
+
+if __name__ == '__main__':
+    url = 'http://www.kanunu8.com/book3/7192/'
+    fish = Bookfish(url)
+    print(fish.chapters)
--- bookfish_tests.py
+++ bookfish_tests.py
@@ -2,21 +2,36 @@
 """Various tests of bookfish module"""
 
 import unittest
-import bookfish
+from bookfish import Bookfish
+
+
 
 # Link to index page of 古典爱情 by 余华
 test_url = 'http://www.kanunu8.com/book3/7192/'
 
+# Bookfish object to be tested
+fish = Bookfish(test_url)
+
 class TestFish(unittest.TestCase):
 
-    def test_find_chapter_urls(self):
+    # def test_get_html(self):
+    #     """Verify that get_html function returns html"""
+    #     raise AssertionError("Tests not yet written")
+
+    def test_right_number_of_chapters(self):
         """Verify that the right amount of chapters are found"""
-        chapters = bookfish.find_chapter_urls(test_url)
+        chapters = fish.find_chapter_urls()
         self.assertEqual(len(chapters), 6)
 
-    def test_no_tags(self):
-        """Verify that all HTML tags have been removed from text"""
-        pass
+    def test_right_form_of_chapter_urls(self):
+        """Verify that chapter urls are of the right form"""
+        chapters = fish.chapters
+        for chapter_url in chapters:
+            self.assertRegex(chapter_url, r'http://www\.kanunu8\.com.*\.html')
+
+    # def test_no_tags(self):
+    #     """Verify that all HTML tags have been removed from text"""
+    #     raise AssertionError("Tests not yet written")
 
 
 if __name__ == "__main__":
