b226119f96c08bb2d8ff22a5337a1dd927c5ef60
el-ethan <elethan@gmail.com>
2015-06-26 22:43:28 -0500

    Add docstrings

--- bookfish.py
+++ bookfish.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
-"""Bookfish"""
+"""
+Bookfish crawls through Chinese novel websites and gives you the novels in plain
+text.
+"""
 import re
 import urllib.request
 
 
 class Bookfish(object):
+    """Bookfish objects are passed a url of an index page for a Chinese novel
+    and will give you the text of that novel as a plain text string, or if
+    print_to_file is set to True, will print the contents of the novel to a
+    file the title of which should match the pattern title_author.
+    """
 
     def __init__(self, url, print_to_file=False):
         self.url = url
@@ -22,6 +30,7 @@ class Bookfish(object):
                 f.write(self.book)
 
     def get_html(self, url):
+        """Return html of url"""
         f = urllib.request.urlopen(url)
         html = f.read()
         html = html.decode('gb18030')
@@ -37,16 +46,18 @@ class Bookfish(object):
         return chapter_urls
 
     def get_title_author(self):
+        """Return title of novel and author"""
         regex = re.compile(r'(?<=<title>)(.*?)\s-\s(.*?)(?=\s-.*</title>)')
         m = regex.search(self.html)
         return m.groups()
 
     def get_book(self):
+        """Return text of novel"""
         html = ''
         # Unicode fish character used as chapter separator
-        fish_char = u'\U0001F41F'
+        ascii_fish ="><(((('>"
         for chapter in self.chapters:
-            html += (fish_char) + self.get_html(chapter)
+            html += (ascii_fish) + self.get_html(chapter)
 
         regex = re.compile(r"""
                            <a.*?</a>        # Links
@@ -69,8 +80,8 @@ class Bookfish(object):
         return book
 
 if __name__ == '__main__':
-    url = input("Enter a URL or press return: ")
-    if not url:
-        url = 'http://www.kanunu8.com/book3/7192/'
-    fish = Bookfish(url)
+    test_url = input("Enter a URL or press return: ")
+    if not test_url:
+        test_url = 'http://www.kanunu8.com/book3/7192/'
+    fish = Bookfish(test_url)
     print(fish.book)
